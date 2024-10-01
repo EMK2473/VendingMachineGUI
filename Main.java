c26cee3534c8588a25f0c0379980e188432714ec
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-10-01 19:42:19 -0400

    removed java scanner and replaced for JOptionPanes; created initial prompt with exit to break application loop;

--- Main.java
+++ Main.java
@@ -1,22 +1,22 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Main {
-   public static void main(String[] args) {
-      Scanner scnr = new Scanner(System.in);
-      
-      int numToPurchase;
-      int numToPurchase2;
-      int numToRestock;
-      
-      numToPurchase = scnr.nextInt();
-      numToRestock = scnr.nextInt();
-      numToPurchase2 = scnr.nextInt();
-      
-      VendingMachine vendingMachine = new VendingMachine();
-      vendingMachine.purchase(numToPurchase);
-      vendingMachine.restock(numToRestock);
-      vendingMachine.purchase(numToPurchase2);
-      
-      vendingMachine.report();
-   }
+    public static void main(String[] args) {
+        VendingMachine vendingMachine = new VendingMachine();
+
+        while (true) {
+            String[] options = {"Customer", "Vendor", "Exit"};
+            int userType = JOptionPane.showOptionDialog(null, 
+                "Choose one: Customer or Vendor", 
+                "Vending Machine", 
+                JOptionPane.DEFAULT_OPTION, 
+                JOptionPane.INFORMATION_MESSAGE, 
+                null, options, options[0]);
+
+                if(userType == 2){
+                  System.exit(0);
+                }
+
+        }
+    }
 }
